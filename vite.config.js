517546e5a5be0513353cd0d980048f1639284c36
Divya Prakash <89380519+divyap3009@users.noreply.github.com>
2024-04-16 16:20:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build',
-  },
 })
-
-// // vite.config.js
-// import { defineConfig } from 'vite';
-
-// export default defineConfig({
-  // build: {
-  //   outDir: 'build',
-  // },
-// });
-
