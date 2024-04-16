69de5dd8089f90541cfa08ddb36e9502876253f3
Divya Prakash <89380519+divyap3009@users.noreply.github.com>
2024-04-16 21:38:18 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -31,7 +31,7 @@ const Projects = () => {
                 >
                     Projects
                 </motion.h2>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8">
                     {projectsData.map((project, index) => (
                         <motion.div
                             key={index}
