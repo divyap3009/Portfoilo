08a9508a91ac3a74742ca42e990472c65e3cfbbc
Divya Prakash <89380519+divyap3009@users.noreply.github.com>
2024-06-21 20:53:02 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -66,7 +66,7 @@ function Home() {
             <Projects />
             <Services />
             <Timeline />
-            <Testimonial />
+{/*             <Testimonial /> */}
             <Contact />
         </>
     );
